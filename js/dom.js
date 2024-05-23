486241fc423b68910992ee9efa3c4daff5b2f7c1
Samestora <putrantosurya38@gmail.com>
2024-05-23 10:02:23 +0700

    adding banner

--- js/dom.js
+++ js/dom.js
@@ -15,3 +15,15 @@ document.getElementById('res-dropbtn').addEventListener('mousedown', function(ev
       event.preventDefault();
     }
 }, false);
+
+document.getElementById('login-btn').addEventListener('mousedown', function(event) {
+  if (event.detail > 1) {
+    event.preventDefault();
+  }
+}, false);
+
+document.getElementById('signup-btn').addEventListener('mousedown', function(event) {
+  if (event.detail > 1) {
+    event.preventDefault();
+  }
+}, false);
